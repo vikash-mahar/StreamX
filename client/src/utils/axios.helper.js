34a9a3e88117b7cf0648e0ae9d10a577f68b4e37
vikash-mahar <vmahar808@gmail.com>
2025-03-12 01:17:29 +0530

    Initial commit

--- client/src/utils/axios.helper.js
+++ client/src/utils/axios.helper.js
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 import { parseErrorMessage } from "./parseErrorMsg";
 
 const axiosInstance = axios.create({
-    baseURL:"https://stream-theta-five.vercel.app/api/v1",
+    baseURL:"http://localhost:5173/api/v1",
     withCredentials: true,
 });
 
