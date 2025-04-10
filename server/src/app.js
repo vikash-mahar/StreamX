8706e4660eb24c9a83d34df2fc3508de357d07ce
vikash-mahar <vmahar808@gmail.com>
2025-04-10 21:55:12 +0530

    Initial commit

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 const app=express()
 
 app.use(cors({
-    origin:["http://localhost:5173",
+    origin:[
     "https://github.com/Sameer0820/Video-Streaming-App/tree/main"],
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
     credentials:true
